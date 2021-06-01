1ebfefe687f6650a7afe19a79e2e633d6aeca49d
thepsalmist <xf.xavierfrank@gmail.com>
2021-06-01 18:28:00 +0300

    create dev and prod settings

---
+++ src/analytica/settings/production.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+DEBUG = config("DEBUG", default=True, cast=bool)
+
+ALLOWED_HOSTS = ["ip-address", "www.myhost.com"]
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": "",
+    }
+}
