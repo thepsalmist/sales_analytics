af6b8261a2762b430ed9ce44e163e61a07494e43
thepsalmist <xf.xavierfrank@gmail.com>
2021-06-02 16:01:08 +0300

    add reports models and admin

--- src/reports/admin.py
+++ src/reports/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Report
 
-# Register your models here.
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ["name", "author", "created_at", "updated_at"]
+    list_filter = ["author"]
--- src/reports/models.py
+++ src/reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from authentication.models import Profile
 
-# Create your models here.
+
+class Report(models.Model):
+    name = models.CharField(max_length=156)
+    image = models.ImageField(default="report.png", upload_to="reports")
+    remarks = models.TextField(help_text="Your remarks")
+    author = models.ForeignKey(Profile, on_delete="models.CASCADE")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
