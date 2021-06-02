422e4aa39b6b7b7aaad9b16a408941e60115cab2
thepsalmist <xf.xavierfrank@gmail.com>
2021-06-02 12:31:11 +0300

    sales app migrtions

--- src/sales/admin.py
+++ src/sales/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Position, Sale, CSV
 
-# Register your models here.
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = [
+        "transaction_id",
+        "customer",
+        "total_price",
+        "salesperson",
+        "created_at",
+    ]
+    list_filter = ["salesperson", "customer"]
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ["product", "quantity", "price"]
+    list_filter = ["product"]
+
+
+@admin.register(CSV)
+class CSVAdmin(admin.ModelAdmin):
+    list_display = ["file_name", "activated", "created_at"]
